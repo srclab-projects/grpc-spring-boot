c8cda58ec25b321f55dd5dc055ede1066d5c3d5d
SrcLab <fredsuvn@163.com>
2021-04-11 16:41:29 +0800

    add grpc-spring-boot-core

---
+++ grpc-spring-boot-core/src/main/kotlin/xyz/srclab/grpc/spring/boot/core/GrpcAutoConfiguration.kt
@@ -0,0 +1,7 @@
+package xyz.srclab.grpc.spring.boot.core
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class GrpcAutoConfiguration {
+}
--- grpc-spring-boot-core/src/test/java/test/xyz/srclab/spring/boot/grpc/web/Starter.java
+++
@@ -1,7 +0,0 @@
-package test.xyz.srclab.spring.boot.grpc.web;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Starter {
-}
